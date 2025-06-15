8fee4f9e325cb724f8393de892ecfa0c204b7c88
Nikita <user38368@st.guap.ru>
2025-06-15 18:31:22 +0300

    Исправил блок регистрации

--- connect.php
+++ connect.php
@@ -1,11 +1,19 @@
-<?php 
-$server= "localhost"  ;
-$user="root" ;
-$password="";
-$DB="kino";
-$dp = mysqli_connect($server, $user, $password, $DB)
-OR die ("Невозможно соединиться с mysql-сервером.
-   Выполнение программы остановлено");
-	mysqli_query($dp, "SET NAMES utf8");
+<?php
+$server = "localhost";
+$user = "root";
+$password = "";
+$DB = "kino";
 
+// Подключение с обработкой ошибок
+$dp = mysqli_connect($server, $user, $password, $DB);
+
+if (!$dp) {
+    error_log("Ошибка подключения к MySQL: " . mysqli_connect_error());
+    die("Ошибка подключения к базе данных. Пожалуйста, попробуйте позже.");
+}
+
+// Установка кодировки
+if (!mysqli_set_charset($dp, "utf8")) {
+    error_log("Ошибка установки кодировки UTF-8: " . mysqli_error($dp));
+}
 ?>
--- pages/register.php
+++ pages/register.php
@@ -1,19 +1,76 @@
 <?php
-session_start();
+// Запускаем сессию и обработчик ошибок
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
 
-if (isset($_SESSION['user'])) {
+// Упрощенный пользовательский обработчик ошибок
+function userErrorHandler($errno, $errmsg, $filename, $linenum) {
+    $dt = date("Y-m-d H:i:s");
+    $errortype = [
+        E_ERROR => 'Фатальная ошибка',
+        E_WARNING => 'Предупреждение',
+        E_PARSE => 'Ошибка разбора',
+        E_NOTICE => 'Уведомление',
+        E_CORE_ERROR => 'Фатальная ошибка ядра',
+        E_CORE_WARNING => 'Предупреждение ядра',
+        E_COMPILE_ERROR => 'Фатальная ошибка компиляции',
+        E_COMPILE_WARNING => 'Предупреждение компиляции',
+        E_USER_ERROR => 'Пользовательская ошибка',
+        E_USER_WARNING => 'Пользовательское предупреждение',
+        E_USER_NOTICE => 'Пользовательское уведомление',
+        E_STRICT => 'Строгий стандарт',
+        E_RECOVERABLE_ERROR => 'Фатальная восстанавливаемая ошибка',
+        E_DEPRECATED => 'Устаревшая функция',
+        E_USER_DEPRECATED => 'Пользовательское устаревание'
+    ];
+
+    $errorType = $errortype[$errno] ?? 'Неизвестная ошибка';
+    $error_msg = "[{$dt}] {$errorType}: {$errmsg} в файле {$filename} на строке {$linenum}\n";
+
+    // Убедимся, что директория существует и доступна для записи
+    $logFile = __DIR__ . "/error.log";
+    if (is_writable(dirname($logFile)) || (!file_exists($logFile) && is_writable(__DIR__))) {
+        error_log($error_msg, 3, $logFile);
+    }
+
+    // Для фатальных ошибок - прерываем выполнение
+    $fatalErrors = [E_ERROR, E_PARSE, E_CORE_ERROR, E_COMPILE_ERROR, E_USER_ERROR, E_RECOVERABLE_ERROR];
+    if (in_array($errno, $fatalErrors, true)) {
+        // Безопасный вывод ошибки
+        echo '<div style="color: red; padding: 10px; margin: 10px; border: 1px solid red;">';
+        echo 'Произошла критическая ошибка. Пожалуйста, попробуйте позже.';
+        echo '</div>';
+        exit(1);
+    }
+}
+set_error_handler("userErrorHandler");
+
+// Проверка авторизации
+if (!empty($_SESSION['user'])) {
     header('Location: profile.php');
     exit();
 }
 
-include '../connect.php';
+// Подключение к базе данных с обработкой ошибок
+$dp = null;
+try {
+    require_once __DIR__ . '/../connect.php';
 
-// Генерация простой математической капчи
-if (!isset($_SESSION['captcha']))
-{
-    generateCaptcha();
+    // Проверяем соединение
+    if (!$dp || mysqli_connect_errno()) {
+        throw new Exception("Ошибка подключения к базе данных: " . mysqli_connect_error());
+    }
+} catch (Exception $e) {
+    error_log($e->getMessage());
+    $_SESSION['message'] = 'Ошибка подключения к базе данных. Пожалуйста, попробуйте позже.';
+    // Не прерываем выполнение, чтобы показать форму
 }
 
+// Генерация капчи
+if (empty($_SESSION['captcha'])) {
+    generateCaptcha();
+}
 
 function generateCaptcha() {
     $num1 = rand(1, 10);
@@ -21,26 +78,104 @@ function generateCaptcha() {
     $_SESSION['captcha'] = $num1 + $num2;
     $_SESSION['captcha_question'] = "$num1 + $num2";
 }
+
+// Функции для валидации данных
+function sanitizeInput($data) {
+    if (!is_string($data)) {
+        return '';
+    }
+    $data = trim($data);
+    $data = stripslashes($data);
+    return htmlspecialchars($data, ENT_QUOTES, 'UTF-8');
+}
+
+function validateName($name) {
+    return is_string($name) && preg_match('/^[A-Za-zА-Яа-яЁё\s]{2,50}$/u', $name);
+}
+
+function validateEmail($email) {
+    return is_string($email) && filter_var($email, FILTER_VALIDATE_EMAIL);
+}
+
+function validatePhone($phone) {
+    return is_string($phone) && preg_match('/^[\+]{0,1}[0-9\s\-\(\)]{10,15}$/', $phone);
+}
+
+function validateLogin($login) {
+    return is_string($login) && preg_match('/^[A-Za-z0-9]{3,10}$/', $login);
+}
+
+function validatePassword($password) {
+    return is_string($password) && strlen($password) >= 4 && strlen($password) <= 16;
+}
+
+function validateDate($date) {
+    return is_string($date) && preg_match('/^\d{4}-\d{2}-\d{2}$/', $date);
+}
+
+// Обработка ошибок при выводе данных из сессии
+function getSessionData($key) {
+    if (!isset($_SESSION['form_data'][$key])) {
+        return '';
+    }
+
+    $value = $_SESSION['form_data'][$key];
+    return is_string($value) ? htmlspecialchars($value, ENT_QUOTES, 'UTF-8') : '';
+}
+
+// Безопасное получение вопроса капчи
+function getCaptchaQuestion() {
+    return isset($_SESSION['captcha_question']) ? htmlspecialchars($_SESSION['captcha_question']) : '';
+}
 ?>
 <!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8">
     <title>Регистрация</title>
-    <!-- CSS -->
     <link rel="icon" type="image/png" href="../img/logo.png">
     <link rel="stylesheet" type="text/css" href="../css/color-text.css">
     <link rel="stylesheet" type="text/css" href="../css/style.css">
     <link rel="stylesheet" type="text/css" href="../css/shadow.css">
-    <!-- CSS end -->
+    <style>
+        nav {
+            display: block;
+            border-radius: 10px;
+            padding-top: 10px;
+            padding-bottom: 10px;
+            background: #E0FFFF;
+            margin-left: -10px;
+            margin-right: -10px;
+            text-align: center;
+            opacity: 90%;
+            width: 50%;
+            margin-left: auto;
+            margin-right: auto;
+        }
+        .a_nav {
+            margin: 7px;
+            color: rgb(8, 54, 28);
+            text-decoration: none;
+        }
+        .error {
+            color: red;
+            font-size: 0.9em;
+            margin-top: 5px;
+        }
+        .msg {
+            color: green;
+            font-size: 0.9em;
+            margin-top: 10px;
+        }
+    </style>
 </head>
 <body>
-<!-- navbar -->
+<!-- Навигация -->
 <div class="navbar">
     <div class="container">
         <div class="navbar-nav">
             <div class="navbar-brand">
-                <a href="../index.php"><img class="navbar-brand-png" src="../img/logo_main.png"></a>
+                <a href="../index.php"><img class="navbar-brand-png" src="../img/logo_main.png" alt="Логотип"></a>
             </div>
             <div class="navs" id="navs">
                 <div class="navs-item notbtn"><a href="galery.php" class="txt-uppercase">Постеры</a></div>
@@ -51,57 +186,74 @@ function generateCaptcha() {
         </div>
     </div>
 </div>
-<!-- navbar end -->
-<!-- Registration form -->
+
+
+<!-- Форма регистрации -->
 <div class="raa">
     <div class="container">
         <div class="features-box">
             <h1 class="features-t">Регистрация</h1>
-            <form action="../RAA/signup.php" method="post" enctype="multipart/form-data" class="raa">
-                <label>Имя</label>
-                <input class="raa" type="text" name="first_name" placeholder="Ваше имя"
-                       value="<?= isset($_SESSION['form_data']['first_name']) ? htmlspecialchars($_SESSION['form_data']['first_name']) : '' ?>"
-                       pattern="[A-Za-zА-Яа-яЁё\s]{2,50}" title="Только буквы (2-50 символов)" required>
+            <form action="../RAA/signup.php" method="post" enctype="multipart/form-data" class="raa" id="registrationForm" novalidate>
+                <!-- Имя -->
+                <label for="first_name">Имя</label>
+                <input class="raa" type="text" name="first_name" id="first_name" placeholder="Ваше имя" required
+                       value="<?= getSessionData('first_name') ?>">
+                <div class="error" id="first_name_error"></div>
 
-                <label>Фамилия</label>
-                <input class="raa" type="text" name="last_name" placeholder="Ваша фамилия"
-                       value="<?= isset($_SESSION['form_data']['last_name']) ? htmlspecialchars($_SESSION['form_data']['last_name']) : '' ?>"
-                       pattern="[A-Za-zА-Яа-яЁё\s]{2,50}" title="Только буквы (2-50 символов)" required>
+                <!-- Фамилия -->
+                <label for="last_name">Фамилия</label>
+                <input class="raa" type="text" name="last_name" id="last_name" placeholder="Ваша фамилия" required
+                       value="<?= getSessionData('last_name') ?>">
+                <div class="error" id="last_name_error"></div>
 
-                <label>Email</label>
-                <input class="raa" type="email" name="email" required placeholder="Ваш Email"
-                       value="<?= isset($_SESSION['form_data']['email']) ? htmlspecialchars($_SESSION['form_data']['email']) : '' ?>">
+                <!-- Email -->
+                <label for="email">Email</label>
+                <input class="raa" type="email" name="email" id="email" required placeholder="Ваш Email"
+                       value="<?= getSessionData('email') ?>">
+                <div class="error" id="email_error"></div>
 
-                <label>Телефон</label>
-                <input class="raa" type="tel" name="phone" placeholder="Ваш телефон"
-                       value="<?= isset($_SESSION['form_data']['phone']) ? htmlspecialchars($_SESSION['form_data']['phone']) : '' ?>"
-                       pattern="[\+]{0,1}[0-9\s\-\(\)]{10,15}" title="Формат: +7 (XXX) XXX-XX-XX">
+                <!-- Телефон -->
+                <label for="phone">Телефон</label>
+                <input class="raa" type="tel" name="phone" id="phone" placeholder="Ваш телефон"
+                       value="<?= getSessionData('phone') ?>">
+                <div class="error" id="phone_error"></div>
 
-                <label>Логин</label>
-                <input class="raa" type="text" name="login" required placeholder="Логин (от 3 до 10 символов)"
-                       value="<?= isset($_SESSION['form_data']['login']) ? htmlspecialchars($_SESSION['form_data']['login']) : '' ?>"
-                       pattern="[A-Za-z0-9]{3,10}" title="Латиница и цифры (3-10 символов)">
+                <!-- Логин -->
+                <label for="login">Логин</label>
+                <input class="raa" type="text" name="login" id="login" required placeholder="Логин (от 3 до 10 символов)"
+                       value="<?= getSessionData('login') ?>">
+                <div class="error" id="login_error"></div>
 
-                <label>Пароль</label>
-                <input class="raa" type="password" name="password" required placeholder="Пароль (от 4 до 16 символов)"
-                       pattern=".{4,16}" title="4-16 символов">
+                <!-- Пароль -->
+                <label for="password">Пароль</label>
+                <input class="raa" type="password" name="password" id="password" required placeholder="Пароль (от 4 до 16 символов)">
+                <div class="error" id="password_error"></div>
 
-                <label>Подтверждение пароля</label>
-                <input class="raa" type="password" name="password_confirm" required placeholder="Подтвердите пароль">
+                <!-- Подтверждение пароля -->
+                <label for="password_confirm">Подтверждение пароля</label>
+                <input class="raa" type="password" name="password_confirm" id="password_confirm" required placeholder="Подтвердите пароль">
+                <div class="error" id="password_confirm_error"></div>
 
-                <!-- Простая математическая капча -->
-                <label>Решите пример: <?= isset($_SESSION['captcha_question']) ? $_SESSION['captcha_question'] : generateCaptcha() ?> = ?</label>
-                <input class="raa" type="text" name="captcha" required placeholder="Введите ответ">
+                <!-- Дата рождения -->
+                <label for="birthdate">Дата рождения</label>
+                <input class="raa" type="date" name="birthdate" id="birthdate" required
+                       value="<?= getSessionData('birthdate') ?>">
+                <div class="error" id="birthdate_error"></div>
+
+                <!-- Капча -->
+                <label for="captcha">Решите пример: <?= getCaptchaQuestion() ?> = ?</label>
+                <input class="raa" type="text" name="captcha" id="captcha" required placeholder="Введите ответ">
+                <div class="error" id="captcha_error"></div>
 
                 <button class="raa" type="submit" name="REGISTR">Зарегистрироваться</button>
 
                 <p>У вас уже есть аккаунт? - <a href="../pages/auth.php" class="raa">авторизируйтесь</a>!</p>
                 <?php
-                if (isset($_SESSION['message'])) {
-                    echo '<p class="msg"> ' . $_SESSION['message'] . ' </p>';
+                if (!empty($_SESSION['message'])) {
+                    echo '<p class="msg">' . htmlspecialchars($_SESSION['message']) . '</p>';
                     unset($_SESSION['message']);
                 }
-                if (isset($_SESSION['form_data'])) {
+                if (!empty($_SESSION['form_data'])) {
                     unset($_SESSION['form_data']);
                 }
                 ?>
@@ -109,7 +261,7 @@ function generateCaptcha() {
         </div>
     </div>
 </div>
-<!-- Registration form end -->
+
 <div class="footer">
     <div class="container">
         <div class="footer-items">
@@ -119,5 +271,90 @@ function generateCaptcha() {
         </div>
     </div>
 </div>
+
+<script>
+    // Клиентская валидация формы
+    document.getElementById('registrationForm').addEventListener('submit', function(e) {
+        e.preventDefault();
+        let valid = true;
+        const errors = {};
+
+        // Валидация имени
+        const firstName = document.getElementById('first_name').value.trim();
+        if (!/^[A-Za-zА-Яа-яЁё\s]{2,50}$/u.test(firstName)) {
+            errors.first_name = 'Только буквы (2-50 символов)';
+            valid = false;
+        }
+
+        // Валидация фамилии
+        const lastName = document.getElementById('last_name').value.trim();
+        if (!/^[A-Za-zА-Яа-яЁё\s]{2,50}$/u.test(lastName)) {
+            errors.last_name = 'Только буквы (2-50 символов)';
+            valid = false;
+        }
+
+        // Валидация email
+        const email = document.getElementById('email').value.trim();
+        if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+            errors.email = 'Некорректный email';
+            valid = false;
+        }
+
+        // Валидация телефона (необязательное поле)
+        const phone = document.getElementById('phone').value.trim();
+        if (phone && !/^[\+]{0,1}[0-9\s\-\(\)]{10,15}$/.test(phone)) {
+            errors.phone = 'Некорректный формат телефона';
+            valid = false;
+        }
+
+        // Валидация логина
+        const login = document.getElementById('login').value.trim();
+        if (!/^[A-Za-z0-9]{3,10}$/.test(login)) {
+            errors.login = 'Латиница и цифры (3-10 символов)';
+            valid = false;
+        }
+
+        // Валидация пароля
+        const password = document.getElementById('password').value;
+        if (password.length < 4 || password.length > 16) {
+            errors.password = 'Длина пароля должна быть 4-16 символов';
+            valid = false;
+        }
+
+        // Подтверждение пароля
+        const passwordConfirm = document.getElementById('password_confirm').value;
+        if (password !== passwordConfirm) {
+            errors.password_confirm = 'Пароли не совпадают';
+            valid = false;
+        }
+
+        // Валидация даты рождения
+        const birthdate = document.getElementById('birthdate').value;
+        if (!birthdate) {
+            errors.birthdate = 'Укажите дату рождения';
+            valid = false;
+        }
+
+        // Валидация капчи
+        const captcha = document.getElementById('captcha').value.trim();
+        if (!captcha) {
+            errors.captcha = 'Введите ответ';
+            valid = false;
+        }
+
+        // Показываем ошибки
+        for (const field in errors) {
+            const errorElement = document.getElementById(field + '_error');
+            if (errorElement) {
+                errorElement.textContent = errors[field] || '';
+            }
+        }
+
+        // Если форма валидна, отправляем ее
+        if (valid) {
+            this.submit();
+        }
+    });
+</script>
 </body>
 </html>
